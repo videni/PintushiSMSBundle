2a8713c7abfac47121e8c02e53183a324be21d76
vidy <videni@foxmail.com>
2017-03-21 10:40:00 +0800

    add service_code_template_id

--- Settings/SmsSettingSchema.php
+++ Settings/SmsSettingSchema.php
@@ -39,10 +39,12 @@ class SmsSettingSchema implements SchemaInterface
                 'account_id' => '',
                 'auth_token' => '',
                 'phone_verification_template_id' => '',
+                'service_code_template_id' => '',
                 'app_id' => '',
             ], $this->defaults))
             ->addAllowedTypes('account_id',['string'])
             ->addAllowedTypes('auth_token',['string'])
+            ->addAllowedTypes('service_code_template_id',['string'])
             ->addAllowedTypes('phone_verification_template_id',['string'])
             ->addAllowedTypes('app_id',['string'])
         ;
@@ -55,25 +57,31 @@ class SmsSettingSchema implements SchemaInterface
     {
         $builder
             ->add('account_id', TextType::class, [
-                'label' => 'newstar.form.settings.sms.account_id',
+                'label' => 'pintushi.form.settings.sms.account_id',
                 'constraints' => [
                     new NotBlank(),
                 ],
             ])
             ->add('auth_token', TextType::class, [
-                'label' => 'newstar.form.settings.sms.auth_token',
+                'label' => 'pintushi.form.settings.sms.auth_token',
                 'constraints' => [
                     new NotBlank(),
                 ],
             ])
             ->add('phone_verification_template_id', TextType::class, [
-                'label' => 'newstar.form.settings.sms.phone_verification_template_id.label',
+                'label' => 'pintushi.form.settings.sms.phone_verification_template_id.label',
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ])
+            ->add('service_code_template_id', TextType::class, [
+                'label' => 'pintushi.form.settings.sms.service_code_template_id.label',
                 'constraints' => [
                     new NotBlank(),
                 ],
             ])
             ->add('app_id', TextType::class, [
-                'label' => 'newstar.form.settings.sms.app_id',
+                'label' => 'pintushi.form.settings.sms.app_id',
                 'constraints' => [
                     new NotBlank(),
                 ],
